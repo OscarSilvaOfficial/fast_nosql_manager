e1363fb28e3fe57772cd59b651e9f9f935404acf
OscarSilvaOfficial <oscarkaka222@gmail.com>
2023-01-28 18:51:12 -0300

    fix: package version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="fast_nosql_manager",
-    version="0.3.2",
+    version="0.3.3",
     author="Oscar da Silva",
     author_email="oscarkaka222@gmail.com",
     description="Um pacote simples para realizar operações no mongoDB",
